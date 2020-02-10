87c2ec87c76d98c4392c5025a1750a95cd2514d5
Vinodha <Work.V03@gmail.com>
2020-02-10 17:59:42 +0530

    Added GJIntegrate

---
+++ GJIntegrate.java
@@ -0,0 +1,9 @@
+public class GJInegrate {
+
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
+    System.out.println("Git and Jenkins Integration was Successful");
+
+ }
+
+}
